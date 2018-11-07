7f9298d2fcb789af58f67789cf4f2bea43a31a55
Alex Lialios <Alexlialios@gmail.com>
2018-11-06 18:40:57 -0800

    my branch work

---
+++ models/schema.js
@@ -0,0 +1,11 @@
+module.exports = function(sequelize, DataTypes) {
+  var Petlist = sequelize.define("Petlist", {
+    pet_id: DataTypes.INTEGER,
+    pet_name: DataTypes.STRING,
+    pet_type:{
+      type:DataTypes.STRING,
+      allowNull:false},
+    pet_description: DataTypes.TEXT,
+  });
+  return Petlist;
+};
--- models/schema.sql
+++ models/schema.sql
@@ -3,3 +3,16 @@ CREATE DATABASE exampledb;
 
 DROP DATABASE IF EXISTS testdb;
 CREATE DATABASE testdb;
+
+create database petfinderdb;
+create table Petlist (
+pet_id int not null auto_increment,
+pet_name varchar(255) null,
+pet_type varchar(255) not null,
+pet_description text not null,
+
+primary key (pet_id);
+
+
+
+);
