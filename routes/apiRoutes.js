1b73c5bb8c6a18eca8142484e54bfabd34cda723
Josh L <joshtlandi@gmail.com>
2018-11-06 20:52:09 -0700

    post route and handlebars files

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,49 @@
 var db = require("../models");
 
+// module.exports = function(app) {
+//   // Get all examples
+//   app.get("/api/examples", function(req, res) {
+//     db.Example.findAll({}).then(function(dbExamples) {
+//       res.json(dbExamples);
+//     });
+//   });
+
+//   // Create a new example
+//   app.post("/api/examples", function(req, res) {
+//     db.Example.create(req.body).then(function(dbExample) {
+//       res.json(dbExample);
+//     });
+//   });
+
+//   // Delete an example by id
+//   app.delete("/api/examples/:id", function(req, res) {
+//     db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+//       res.json(dbExample);
+//     });
+//   });
+// };
+//^^^^^^^^^^^^^^^^EXAMPLES^^^^^^^^^^^^^^^^^^^^^^^^^
+// REAL CODE
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
-  });
-
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
-
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
+  app.post("/iFoundAPet", function(req, res) {
+    var body = req.body;
+    console.log(body);
+    //create post in DB then redirect user to Post page
+    db.petfinder_data
+      .create({
+        animalSpecies: body.animalSpecies,
+        animalGenderMale: body.animalGenderMale,
+        animalFeatures: body.animalFeatures,
+        animalPic: body.animalPic,
+        foundStreet1: body.foundStreet1,
+        foundStreet2: body.foundStreet2,
+        personName: body.personName,
+        personEmail: body.personEmail
+      })
+      .then(function(data) {
+        // res.json(dbPetFinder);
+        console.log("data", data);
+        res.redirect("/post/" + data.dataValues.id);
+      });
   });
 };
--- server.js
+++ server.js
@@ -15,7 +15,6 @@ app.use(express.static("public"));
 
 // Handlebars
 app.engine(
-  // miguel added this line to change the file
   "handlebars",
   exphbs({
     defaultLayout: "main"
@@ -27,7 +26,7 @@ app.set("view engine", "handlebars");
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
-var syncOptions = { force: false };
+var syncOptions = { force: true };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
