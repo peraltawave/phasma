29a8c2c167367274c6fd35e861db446732439c57
peraltawave <peraltawave@gmail.com>
2018-11-13 16:35:59 -0700

    config only

--- models/index.js
+++ models/index.js
@@ -5,7 +5,7 @@ var path = require("path");
 var Sequelize = require("sequelize");
 var basename = path.basename(module.filename);
 var env = process.env.NODE_ENV || "production";
-var config = require(__dirname + "../config/config.json")[env];
+var config = require(__dirname + "config/config.json")[env];
 var db = {};
 console.log(config.use_env_variable);
 console.log("!!!!!!!! > " + process.env[config.use_env_variable])
